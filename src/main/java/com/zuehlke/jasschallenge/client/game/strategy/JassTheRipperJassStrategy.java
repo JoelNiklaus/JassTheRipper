525b850ca78f05a0f61c5daa2a65f2f7c5c1dac1
Joel Niklaus <me@joelniklaus.ch>
2017-04-12 23:22:54 +0100

    some small work

--- src/main/java/com/zuehlke/jasschallenge/client/game/strategy/JassTheRipperJassStrategy.java
+++ src/main/java/com/zuehlke/jasschallenge/client/game/strategy/JassTheRipperJassStrategy.java
@@ -10,31 +10,47 @@ import weka.classifiers.functions.MultilayerPerceptron;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
+import java.util.stream.Stream;
 
 
 // TODO Only ML methods or include Jass Knowledge?
 public class JassTheRipperJassStrategy implements JassStrategy {
 
-    private MultilayerPerceptron mlp = new MultilayerPerceptron();
+	private MultilayerPerceptron mlp = new MultilayerPerceptron();
 
-    @Override
-    public Mode chooseTrumpf(Set<Card> availableCards, GameSession session, boolean isGschobe) {
-        final List<Mode> allPossibleModes = Mode.standardModes();
-        if (!isGschobe) {
-            allPossibleModes.add(Mode.shift());
-        }
-        return allPossibleModes.get(new Random().nextInt(allPossibleModes.size()));
-    }
+	@Override
+	public Mode chooseTrumpf(Set<Card> availableCards, GameSession session, boolean isGschobe) {
+		final List<Mode> allPossibleModes = Mode.standardModes();
+		if (!isGschobe) {
+			allPossibleModes.add(Mode.shift());
+		}
+		return allPossibleModes.get(new Random().nextInt(allPossibleModes.size()));
+	}
 
-    @Override
-    public Card chooseCard(Set<Card> availableCards, GameSession session) {
-        final Game currentGame = session.getCurrentGame();
-        final Round round = currentGame.getCurrentRound();
-        final Mode gameMode = round.getMode();
+	@Override
+	public Card chooseCard(Set<Card> availableCards, GameSession session) {
+		final Game currentGame = session.getCurrentGame();
+		final Round round = currentGame.getCurrentRound();
+		final Mode gameMode = round.getMode();
 
-        return availableCards.stream()
-                    .filter(card -> gameMode.canPlayCard(card, round.getPlayedCards(), round.getRoundColor(), availableCards))
-                    .findAny()
-                    .orElseThrow(() -> new RuntimeException("There should always be a card to play"));
-    }
+		return getPossibleCards(availableCards, round, gameMode)
+				.findAny()
+				.orElseThrow(() -> new RuntimeException("There should always be a card to play"));
+	}
+
+	private Stream<Card> getPossibleCards(Set<Card> availableCards, Round round, Mode gameMode) {
+		return availableCards.stream().filter(card -> gameMode.canPlayCard(card, round.getPlayedCards(), round.getRoundColor(), availableCards));
+	}
+
+	private void mitTrumpfAbstechen() {
+
+	}
+
+	private void mitNichtTrumpfStechen() {
+
+	}
+
+	private void verwerfen() {
+		
+	}
 }
