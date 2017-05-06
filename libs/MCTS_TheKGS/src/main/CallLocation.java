a9fc7f1af8e1ce8037ab8c0bfd90042f5240ef7e
dombriner <dominik.briner@students.unibe.ch>
2017-05-06 17:18:48 +0200

    Added MCTS library of TheKGS

--- libs/MCTS_TheKGS/src/main/CallLocation.java
+++ libs/MCTS_TheKGS/src/main/CallLocation.java
@@ -3,4 +3,5 @@ package main;
 public enum CallLocation {
 	treePolicy,
 	playout
+
 }
--- libs/MCTS_TheKGS/src/main/FinalSelectionPolicy.java
+++ libs/MCTS_TheKGS/src/main/FinalSelectionPolicy.java
@@ -3,4 +3,5 @@ package main;
 public enum FinalSelectionPolicy {
 	maxChild, // The child with highest SCORE
 	robustChild // The child with highest visit count
+
 }
