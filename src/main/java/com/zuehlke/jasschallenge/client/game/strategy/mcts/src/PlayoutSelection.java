293fe364dab2bbf7255a7ca4833d8eeaa9eb6e28
Joel Niklaus <me@joelniklaus.ch>
2017-05-10 14:20:36 +0100

    added PlayoutSelection interface

---
+++ src/main/java/com/zuehlke/jasschallenge/client/game/strategy/mcts/src/PlayoutSelection.java
@@ -0,0 +1,14 @@
+package com.zuehlke.jasschallenge.client.game.strategy.mcts.src;
+
+/**
+ * Use this as a replacement for the conventional
+ * playout function during simulations. The idea
+ * is to implement a function that takes a game
+ * board, applies some random available move from
+ * the current state and returns it.
+ *
+ * @author KGS
+ */
+public interface PlayoutSelection {
+	public void process(Board board);
+}
