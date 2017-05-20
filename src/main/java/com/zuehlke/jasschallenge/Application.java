c161581a140897060d7d8232c4c84b9c7b21b0b4
Joel Niklaus <me@joelniklaus.ch>
2017-05-20 18:44:20 +0100

    some refactoring

--- src/main/java/com/zuehlke/jasschallenge/Application.java
+++ src/main/java/com/zuehlke/jasschallenge/Application.java
@@ -19,46 +19,50 @@ import java.util.Arrays;
  * </pre>
  */
 public class Application {
-    //CHALLENGE2017: Set your bot name
-    private static final String BOT_NAME = "JassTheRipper";
-    //CHALLENGE2017: Set your own strategy
-    private static final JassTheRipperJassStrategy STRATEGY = new JassTheRipperJassStrategy();
+	//CHALLENGE2017: Set your bot name
+	private static final String BOT_NAME = "JassTheRipper";
+	//CHALLENGE2017: Set your own strategy
+	private static final JassTheRipperJassStrategy STRATEGY = new JassTheRipperJassStrategy();
 
-    private static final String LOCAL_URL = "ws://127.0.0.1:3000";
+	private static final String LOCAL_URL = "ws://127.0.0.1:3000";
 
-    public static void main(String[] args) throws Exception {
-        String websocketUrl = parseWebsocketUrlOrDefault(args);
+	public static void main(String[] args) throws Exception {
+		// Competition
+		String websocketUrl = parseWebsocketUrlOrDefault(args);
 
-        Player jassTheRipper = new Player(BOT_NAME, STRATEGY);
-        Player jassTheRipperRandomTrumpf = new Player("JassTheRipperRandomTrumpf", new JassTheRipperRandomTrumpfJassStrategy());
-        Player jassTheRipperRandomCard = new Player("JassTheRipperRandomCard", new JassTheRipperRandomCardJassStrategy());
+		System.out.println("Connecting... Server socket URL: " + websocketUrl);
+
+		Player player = new Player(BOT_NAME, STRATEGY);
+		startGame(websocketUrl, player, SessionType.TOURNAMENT);
+
+
+		// Testing
+		Player jassTheRipper = new Player("JassTheRipper", new JassTheRipperJassStrategy());
+		Player jassTheRipperRandomTrumpf = new Player("JassTheRipperRandomTrumpf", new JassTheRipperRandomTrumpfJassStrategy());
+		Player jassTheRipperRandomCard = new Player("JassTheRipperRandomCard", new JassTheRipperRandomCardJassStrategy());
 		Player randomJasser = new Player("RandomJasser", new RandomJassStrategy());
 
-
-        System.out.println("Connecting... Server socket URL: " + websocketUrl);
-
-        // Change here to run different bot
-        //startGame(websocketUrl, jassTheRipper, SessionType.SINGLE_GAME);
+		// Change here to run different bot
+		//startGame(websocketUrl, jassTheRipper, SessionType.SINGLE_GAME);
 
 
 		//startGame(websocketUrl, randomJasser, SessionType.TOURNAMENT);
-		startGame(websocketUrl, jassTheRipper, SessionType.TOURNAMENT);
-		//startGame(websocketUrl, jassTheRipperRandomTrumpf, SessionType.TOURNAMENT);
-        //startGame(websocketUrl, jassTheRipperRandomCard, SessionType.TOURNAMENT);
+		//startGame(websocketUrl, jassTheRipper, SessionType.TOURNAMENT);
+		startGame(websocketUrl, jassTheRipperRandomTrumpf, SessionType.TOURNAMENT);
+		//startGame(websocketUrl, jassTheRipperRandomCard, SessionType.TOURNAMENT);
 
-    }
+	}
 
+	private static String parseWebsocketUrlOrDefault(String[] args) {
+		if (args.length > 0) {
+			System.out.println("Arguments: " + Arrays.toString(args));
+			return args[0];
+		}
+		return LOCAL_URL;
+	}
 
-    private static String parseWebsocketUrlOrDefault(String[] args) {
-        if (args.length > 0) {
-            System.out.println("Arguments: " + Arrays.toString(args));
-            return args[0];
-        }
-        return LOCAL_URL;
-    }
-
-    private static void startGame(String targetUrl, Player myLocalPlayer, SessionType sessionType) throws Exception {
-        RemoteGame remoteGame = new RemoteGame(targetUrl, myLocalPlayer, sessionType);
-        remoteGame.start();
-    }
+	private static void startGame(String targetUrl, Player myLocalPlayer, SessionType sessionType) throws Exception {
+		RemoteGame remoteGame = new RemoteGame(targetUrl, myLocalPlayer, sessionType);
+		remoteGame.start();
+	}
 }
