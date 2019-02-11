f0c115f64162427eadcef772fb60efea2fa2b3ba
Joel Niklaus <me@joelniklaus.ch>
2019-02-11 20:36:48 +0800

    performed some refactoring

--- src/main/java/com/zuehlke/jasschallenge/client/game/PlayingOrder.java
+++ src/main/java/com/zuehlke/jasschallenge/client/game/PlayingOrder.java
@@ -7,8 +7,8 @@ import java.util.List;
 public class PlayingOrder implements Serializable {
 
 	private final List<Player> playersInInitialPlayingOrder;
-	private final int startingPlayer;
-	private int currentPlayer;
+	private final int startingPlayerInt;
+	private int currentPlayerInt;
 
 	public static PlayingOrder createOrder(List<Player> playersInInitialPlayingOrder) {
 		return new PlayingOrder(playersInInitialPlayingOrder, 0);
@@ -18,10 +18,10 @@ public class PlayingOrder implements Serializable {
 		return new PlayingOrder(playersInPlayingOrder, playersInPlayingOrder.indexOf(startFrom));
 	}
 
-	private PlayingOrder(List<Player> playersInInitialPlayingOrder, int startingPlayer) {
+	private PlayingOrder(List<Player> playersInInitialPlayingOrder, int startingPlayerInt) {
 		this.playersInInitialPlayingOrder = playersInInitialPlayingOrder;
-		this.startingPlayer = startingPlayer;
-		this.currentPlayer = 0;
+		this.startingPlayerInt = startingPlayerInt;
+		this.currentPlayerInt = 0;
 	}
 
 	// TODO maybe order is confused
@@ -35,8 +35,8 @@ public class PlayingOrder implements Serializable {
 			this.playersInInitialPlayingOrder = new ArrayList<>();
 			for (Player player : playingOrder.getPlayersInInitialPlayingOrder())
 				this.playersInInitialPlayingOrder.add(new Player(player));
-			this.startingPlayer = playingOrder.getStartingPlayer();
-			this.currentPlayer = playingOrder.getCurrentPlayerInt();
+			this.startingPlayerInt = playingOrder.getStartingPlayerInt();
+			this.currentPlayerInt = playingOrder.getCurrentPlayerInt();
 		}
 	}
 
@@ -44,8 +44,8 @@ public class PlayingOrder implements Serializable {
 		return playersInInitialPlayingOrder;
 	}
 
-	public int getStartingPlayer() {
-		return startingPlayer;
+	public int getStartingPlayerInt() {
+		return startingPlayerInt;
 	}
 
 	public List<Player> getPlayerInOrder() {
@@ -53,28 +53,28 @@ public class PlayingOrder implements Serializable {
 	}
 
 	public Player getCurrentPlayer() {
-		return playersInInitialPlayingOrder.get(getBoundIndex(currentPlayer));
+		return playersInInitialPlayingOrder.get(getBoundIndex(currentPlayerInt));
 	}
 
 	public void moveToNextPlayer() {
-		currentPlayer = currentPlayer + 1;
+		currentPlayerInt++;
 	}
 
 	private int getBoundIndex(int playerPosition) {
-		return (this.startingPlayer + playerPosition) % playersInInitialPlayingOrder.size();
+		return (this.startingPlayerInt + playerPosition) % playersInInitialPlayingOrder.size();
 	}
 
 	@Override
 	public String toString() {
 		return "PlayingOrder{" +
 				"playersInInitialPlayingOrder=" + playersInInitialPlayingOrder +
-				", startingPlayer=" + startingPlayer +
-				", currentPlayer=" + currentPlayer +
+				", startingPlayerInt=" + startingPlayerInt +
+				", currentPlayerInt=" + currentPlayerInt +
 				'}';
 	}
 
 	public int getCurrentPlayerInt() {
-		return currentPlayer;
+		return currentPlayerInt;
 	}
 
 	@Override
@@ -84,16 +84,16 @@ public class PlayingOrder implements Serializable {
 
 		PlayingOrder that = (PlayingOrder) o;
 
-		if (startingPlayer != that.startingPlayer) return false;
-		if (currentPlayer != that.currentPlayer) return false;
+		if (startingPlayerInt != that.startingPlayerInt) return false;
+		if (currentPlayerInt != that.currentPlayerInt) return false;
 		return playersInInitialPlayingOrder != null ? playersInInitialPlayingOrder.equals(that.playersInInitialPlayingOrder) : that.playersInInitialPlayingOrder == null;
 	}
 
 	@Override
 	public int hashCode() {
 		int result = playersInInitialPlayingOrder != null ? playersInInitialPlayingOrder.hashCode() : 0;
-		result = 31 * result + startingPlayer;
-		result = 31 * result + currentPlayer;
+		result = 31 * result + startingPlayerInt;
+		result = 31 * result + currentPlayerInt;
 		return result;
 	}
 }
